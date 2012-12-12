e9a034a485bc4bb73bee5269bf49263c74e5aaee
Bernhard Dieber <bernhard.dieber@aau.at>
2012-12-12 10:31:34 +0100

    added class for network stub

---
+++ middleware/middleware/Network/Stub.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ella.Attributes;
+using Ella.Data;
+
+namespace Ella.Network
+{
+    [Publishes(typeof (Unknown), 1,CopyPolicy = DataCopyPolicy.None)]
+    internal class Stub
+    {
+        [Start]
+       public void Start()
+       {
+           
+       }
+
+        [Stop]
+        public void Stop()
+        {
+            
+        }
+
+        [Factory]
+        public Stub CreateInstance()
+        {
+            return new Stub();
+        }
+    }
+}
