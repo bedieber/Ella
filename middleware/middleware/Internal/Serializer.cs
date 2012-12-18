92e9e331f457ba268a1159129a6c6b19fdd77821
Bernhard Dieber <bernhard.dieber@aau.at>
2012-12-18 16:00:29 +0100

    Added serialization helper

---
+++ middleware/middleware/Internal/Serializer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace Ella.Internal
+{
+    internal class Serializer
+    {
+        internal static BinaryFormatter _formatter=new BinaryFormatter();
+
+        /// <summary>
+        /// Serializes the specified data.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        /// <returns>A byte[] containing the serialized <paramref name="data"/></returns>
+        internal static byte[] Serialize(object data)
+        {
+            MemoryStream ms=new MemoryStream();
+            _formatter.Serialize(ms, data);
+            ms.Seek(0, SeekOrigin.Begin);
+            return ms.ToArray();
+
+        }
+
+        /// <summary>
+        /// Deserializes the specified data.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data">The data.</param>
+        /// <returns>The deserialized object of type <typeparamref name="T"/></returns>
+        internal static T Deserialize<T>(byte[] data)
+        {
+            MemoryStream ms=new MemoryStream(data);
+            ms.Seek(0, SeekOrigin.Begin);
+            T result = (T) _formatter.Deserialize(ms);
+            return result;
+        }
+
+        /// <summary>
+        /// Performs copy via serialization.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data">The data.</param>
+        /// <returns>A clone of <paramref name="data"/></returns>
+        internal static T SerializeCopy<T>(T data)
+        {
+            MemoryStream ms=new MemoryStream();
+            _formatter.Serialize(ms, data);
+            ms.Seek(0, SeekOrigin.Begin);
+            T result = (T)_formatter.Deserialize(ms);
+            return result;
+        }
+    }
+}
